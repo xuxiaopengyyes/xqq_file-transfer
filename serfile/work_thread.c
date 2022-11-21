f6407b86298b83617ce5edf0f0c4a98c924bab4f
xqq <15591388167@163.com>
2022-11-21 11:16:38 +0800

    makefile

---
+++ serfile/work_thread.c
@@ -0,0 +1,32 @@
+#include"work_thread.h"
+
+void * work_fun(void *arg)
+{
+	int c=(int) arg;
+	while(1)
+	{
+		char buff[128}={0};
+		int num =recv(c,buff,127,0);
+		if(num<0)
+		{
+			break;
+		}
+
+		//测试
+		printf("%s",buff);
+		send("ok",2);	
+	
+		//测试
+	}
+}
+
+void start_thread(int c)
+{
+	phread_t id;
+	if(pthread_create(&id,NULL,work_fun,(void *)c) !=0)
+	{
+		close(c);
+		printf("启动线程失败\n");
+		return ;
+	}
+}
---
+++ serfile/work_thread.h
@@ -0,0 +1,16 @@
+#ifndef WORK_PTHREAD_H
+#define WORK_PTHREAD_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+
+void start_thread(int c);
+
+
+#endif
